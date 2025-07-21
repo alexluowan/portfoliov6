0d0841681c9ae927a8d4c74b0ca45b5429f71c79
Aine <aaluowan@gmail.com>
2025-07-21 00:23:16 -0700

    added analytics for vercel

--- src/app/(index)/layout.tsx
+++ src/app/(index)/layout.tsx
@@ -1,7 +1,7 @@
 import type {Metadata} from "next";
 import "../globals.css";
 import localFont from 'next/font/local'
-
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
     title: "Alex Luowans Portfolio",
